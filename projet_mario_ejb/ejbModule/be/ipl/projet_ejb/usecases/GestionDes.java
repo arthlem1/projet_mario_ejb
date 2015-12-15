875607df16e01c40f335aefdd91f82b27b90574c
kmathy <kevin.mathy95@gmail.com>
2015-12-15 17:17:51 +0100

    Dernier commit fin de journée

--- projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecases/GestionDes.java
+++ projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecases/GestionDes.java
@@ -1,18 +1,8 @@
 package be.ipl.projet_ejb.usecases;
 
-import java.util.List;
-
 import be.ipl.projet_ejb.domaine.De;
-import be.ipl.projet_ejb.domaine.Joueur;
 
 public interface GestionDes {
-	
-	/**
-	 * Recherche dans la DB
-	 * @param joueur possédant les dés
-	 * @return liste des dés
-	 */
-	List<De> listerDes(Joueur joueur);
 	
 	/**
 	 * Recherche dans la DB.
--- projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecasesimpl/GestionDesImpl.java
+++ projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecasesimpl/GestionDesImpl.java
@@ -1,14 +1,11 @@
 package be.ipl.projet_ejb.usecasesimpl;
 
-import java.util.List;
-
 import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
 import be.ipl.projet_ejb.daoimpl.DeDaoImpl;
 import be.ipl.projet_ejb.domaine.De;
-import be.ipl.projet_ejb.domaine.Joueur;
 import be.ipl.projet_ejb.usecases.GestionDes;
 import be.ipl.projet_ejb.util.Util;
 
@@ -17,12 +14,6 @@ import be.ipl.projet_ejb.util.Util;
 public class GestionDesImpl implements GestionDes {
 
 	@EJB DeDaoImpl	deDao;
-	
-	@Override
-	public List<De> listerDes(Joueur joueur) {
-		Util.checkObject(joueur);
-		return deDao.lister(joueur);
-	}
 
 	@Override
 	public De recherche(De de) {
