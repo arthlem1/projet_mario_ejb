839901461520708366b81873c5f11636d69767e7
Laetitia <gillieauxlaetitia@gmail.com>
2015-12-15 15:37:13 +0100

    PartieDaoImpl

--- projet_mario_ejb/ejbModule/be/ipl/projet_ejb/daoimpl/PartieDaoImpl.java
+++ projet_mario_ejb/ejbModule/be/ipl/projet_ejb/daoimpl/PartieDaoImpl.java
@@ -1,10 +1,11 @@
 package be.ipl.projet_ejb.daoimpl;
 
 import java.util.List;
+import java.util.Random;
 
 import be.ipl.projet_ejb.domaine.Joueur;
-import be.ipl.projet_ejb.domaine.Partie;
 import be.ipl.projet_ejb.domaine.JoueurPartie;
+import be.ipl.projet_ejb.domaine.Partie;
 
 public class PartieDaoImpl extends DaoImpl<String, Partie> {
 	//TODO tirerJoueurAuHasard Laeti
@@ -12,8 +13,32 @@ public class PartieDaoImpl extends DaoImpl<String, Partie> {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public PartieDaoImpl() {
-		super(Partie.class);
+	public PartieDaoImpl(Class<Partie> entityClass) {
+		super(entityClass);
+	}
+
+	public Partie rechercher(String nom) {
+		String queryString = "select p from Partie p where p.nom = ?1";
+		return recherche(queryString, nom);
+	}
+
+	public Partie creerPartie(String nom, Joueur createur) {
+		return enregistrer(new Partie(nom, createur));
+	}
+
+	public Partie commencerPartie(String nom) {
+		Partie partie = rechercher(nom);
+		JoueurPartie courant = tirerJoueurAuHasard(partie);
+		partie.setJoueur_courant(courant);
+		partie.setStarted(true);
+		return mettreAJour(partie);
+	}
+
+	public JoueurPartie tirerJoueurAuHasard(Partie partie) {
+		List<JoueurPartie> lesJoueurs = partie.getListeJoueurs();
+		Random random = new Random();
+		int nb = random.nextInt(lesJoueurs.size()) + 1;
+		return lesJoueurs.get(nb);
 	}
 	
 	Partie passerAuJoueurSuivant(Partie partie, JoueurPartie suivant){
