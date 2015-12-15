887ff120c9e42f20697461b46cc4b207f87c3e82
kmathy <kevin.mathy95@gmail.com>
2015-12-15 11:34:28 +0100

    Sauvetage

---
+++ projet_mario_web/src/be/ipl/servlets/FiltreControleur.java
@@ -0,0 +1,45 @@
+package be.ipl.servlets;
+
+import java.io.IOException;
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(filterName="FiltreControleur", urlPatterns = "/*", dispatcherTypes = {DispatcherType.REQUEST} )
+public class FiltreControleur implements Filter {
+	private FilterConfig fConfig;
+	
+	public void init(FilterConfig fConfig) throws ServletException {
+		this.fConfig = fConfig;
+	}
+
+	public void destroy() {
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		if (! (request instanceof HttpServletRequest)) {
+			String message = "Ce Site n'est accessible qu'en HTTP";
+			request.setAttribute("message", message);
+			request.getServletContext().getNamedDispatcher("ErreurParam").forward(request, response);
+			return;
+		}
+		HttpServletRequest req = (HttpServletRequest) request;
+		if (!req.isRequestedSessionIdFromCookie()) {
+			String message = "Pour le bon fonctionnement de ce site, vous devez accepter les cookies.<br> Si vous ne les accetez pas vous ne pourrez pas dépasser la page d'accueil.";
+			request.setAttribute("messageAcceuil", message);
+			request.getServletContext().getNamedDispatcher("index.html").forward(request, response);
+			return;
+		} else
+			request.removeAttribute("messageAccueil");
+		/*NE PAS TOUCHER A LA PARTIE DU DESSUS*/
+		
+		chain.doFilter(request, response);
+	}
+
+}
