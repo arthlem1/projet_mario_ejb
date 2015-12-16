a27d8c56b36a5c7f84939fc65d82e0c7ab1c4984
kmathy <kevin.mathy95@gmail.com>
2015-12-16 22:12:01 +0100

    Javadoc UCC

--- projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecases/GestionJoueurs.java
+++ projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecases/GestionJoueurs.java
@@ -6,7 +6,26 @@ import be.ipl.projet_ejb.domaine.Joueur;
 @Remote
 public interface GestionJoueurs {
 
+	/**
+	 * Créer un nouveau joueur dans la base de données. Le mot de passe
+	 * est crypté dans la DB.
+	 * @param prenom
+	 * @param pseudo
+	 * @param mdp
+	 * @return joueur créé dans la DB
+	 */
 	Joueur creerJoueur(String prenom,String pseudo, String mdp);
+	
+	/**
+	 * @param pseudo du joueur à trouver
+	 * @return joueur trouvé dans la DB
+	 */
 	Joueur rechercher(String pseudo);
+	
+	/**
+	 * @param pseudo
+	 * @param mdp
+	 * @return joueur connecté, null si problème dans login
+	 */
 	Joueur login(String pseudo, String mdp);
 }
