34ed210a1cf439e9370bb132aaacca53bc6fcda3
Kahell <kahell@hotmail.be>
2015-12-16 12:39:35 +0100

    AjouterJoueur By Enum impl

--- projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecasesimpl/GestionPartiesImpl.java
+++ projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecasesimpl/GestionPartiesImpl.java
@@ -6,27 +6,37 @@ import javax.ejb.EJB;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
+import be.ipl.projet_ejb.daoimpl.PartieDaoImpl;
 import be.ipl.projet_ejb.domaine.Joueur;
 import be.ipl.projet_ejb.domaine.JoueurPartie;
 import be.ipl.projet_ejb.domaine.Partie;
 import be.ipl.projet_ejb.usecases.GestionParties;
 import be.ipl.projet_ejb.util.Util;
-import be.ipl.projet_ejb.daoimpl.PartieDaoImpl;
 
 @Singleton
 @Startup
 public class GestionPartiesImpl implements GestionParties {
 
-	@EJB PartieDaoImpl partieDao;
-	
-	enum Etat{
-		INITIAL{
-			boolean ajouterJoueur(Joueur joueur){
-				return true;
+	@EJB
+	PartieDaoImpl partieDao;
+
+	private Etat value = Etat.INITIAL;
+
+	public enum Etat {
+		INITIAL {
+			boolean ajouterJoueur(Joueur joueur, Partie partie, GestionPartiesImpl gpi) {
+				Util.checkObject(joueur);
+				Util.checkObject(partie);
+				return gpi.partieDao.ajouterJoueur(partie, joueur);
 			}
-		},EN_COURS,FINI;
+		},
+		EN_COURS, FINI;
+
+		boolean ajouterJoueur(Joueur joueur, Partie partie, GestionPartiesImpl gpi) {
+			return false;
+		}
 	}
-	
+
 	@Override
 	public void creerPartie(String nom, Joueur createur) {
 		Util.checkString(nom);
@@ -73,9 +83,7 @@ public class GestionPartiesImpl implements GestionParties {
 
 	@Override
 	public boolean ajouterJoueur(Partie partie, Joueur joueur) {
-		Util.checkObject(joueur);
-		Util.checkObject(partie);
-		return partieDao.ajouterJoueur(partie, joueur);
+		return value.ajouterJoueur(joueur, partie, this);
 	}
-	
+
 }
