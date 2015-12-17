18900e9a30ba0d3afb47e4272b445a4c8bd930a3
kmathy <kevin.mathy95@gmail.com>
2015-12-17 09:50:37 +0100

    petit rectificatif de la startégie pioche de trois cartes

--- projet_mario_ejb/ejbModule/be/ipl/projet_ejb/strategy/StrategyPiocheTroisCartes.java
+++ projet_mario_ejb/ejbModule/be/ipl/projet_ejb/strategy/StrategyPiocheTroisCartes.java
@@ -1,5 +1,7 @@
 package be.ipl.projet_ejb.strategy;
 
+import java.util.List;
+
 import be.ipl.projet_ejb.daoimpl.DeDaoImpl;
 import be.ipl.projet_ejb.daoimpl.JoueurPartieDaoImpl;
 import be.ipl.projet_ejb.daoimpl.PartieDaoImpl;
@@ -12,8 +14,15 @@ public class StrategyPiocheTroisCartes implements Strategy {
 	@Override
 	public void effectuer(DeDaoImpl deDao, PartieDaoImpl partieDao, JoueurPartieDaoImpl joueurPartieDao, Partie partie,
 			Joueur joueur, Joueur cible) {
-		Carte carte = partie.getPioche().remove(0);
-		joueurPartieDao.rajouterCarte(joueur, partie, carte);
+		List<Carte> pioche = partie.getPioche();
+		for(int i = 0; i< 3; i++){
+			if (pioche.size() == 0) {
+				/* TODO à discuter. Selon les consignes, si plus de carte dans la
+	 			pioche, prendre une carte dans la main d'un autre*/
+			}
+			Carte carte = pioche.remove(0);// premiere carte
+			joueurPartieDao.rajouterCarte(joueur, partie, carte);
+		}
 	}
 
 }
