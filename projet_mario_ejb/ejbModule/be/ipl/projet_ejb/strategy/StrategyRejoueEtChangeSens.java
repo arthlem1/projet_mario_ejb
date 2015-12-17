5ec2a9fae341fe2d8d542b9a8e8e93062b937e15
Kahell <kahell@hotmail.be>
2015-12-17 11:23:41 +0100

    com

--- projet_mario_ejb/ejbModule/be/ipl/projet_ejb/strategy/StrategyRejoueEtChangeSens.java
+++ projet_mario_ejb/ejbModule/be/ipl/projet_ejb/strategy/StrategyRejoueEtChangeSens.java
@@ -11,7 +11,8 @@ public class StrategyRejoueEtChangeSens implements Strategy {
 	@Override
 	public void effectuer(DeDaoImpl deDao, PartieDaoImpl partieDao, JoueurPartieDaoImpl joueurPartieDao, Partie partie,
 			Joueur joueur, Joueur cible) {
-		partieDao.setJoueurSuivant(partie,joueur);
+		
+		//partieDao.setJoueurSuivant(partie,joueur);
 		partieDao.changerSens(partie);
 	}
 
