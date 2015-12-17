62e2cf362322acb6c6b47de16acefd7d0970d9fe
Kahell <kahell@hotmail.be>
2015-12-17 10:00:54 +0100

    Implement replay on Strategy

--- projet_mario_ejb/ejbModule/be/ipl/projet_ejb/strategy/StrategyRejoueEtChangeSens.java
+++ projet_mario_ejb/ejbModule/be/ipl/projet_ejb/strategy/StrategyRejoueEtChangeSens.java
@@ -5,13 +5,15 @@ import be.ipl.projet_ejb.daoimpl.JoueurPartieDaoImpl;
 import be.ipl.projet_ejb.daoimpl.PartieDaoImpl;
 import be.ipl.projet_ejb.domaine.Joueur;
 import be.ipl.projet_ejb.domaine.Partie;
+import be.ipl.projet_ejb.domaine.JoueurPartie;
 
 public class StrategyRejoueEtChangeSens implements Strategy {
 
 	@Override
 	public void effectuer(DeDaoImpl deDao, PartieDaoImpl partieDao, JoueurPartieDaoImpl joueurPartieDao, Partie partie,
 			Joueur joueur, Joueur cible) {
-		//TODO faire rejouer un joueur
+		JoueurPartie jp= joueurPartieDao.getPlayer(joueur.getId(), partie.getId());
+		partieDao.passerAuJoueurSuivant(partie, jp);
 		partieDao.changerSens(partie);
 	}
 
