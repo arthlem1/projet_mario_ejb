b1eef480785ec463fb61528f17e882dec46cd917
kmathy <kevin.mathy95@gmail.com>
2015-12-18 10:46:10 +0100

    merge GestionPartieImpl

--- projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecasesimpl/GestionPartiesImpl.java
+++ projet_mario_ejb/ejbModule/be/ipl/projet_ejb/usecasesimpl/GestionPartiesImpl.java
@@ -1,6 +1,7 @@
 package be.ipl.projet_ejb.usecasesimpl;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -180,9 +181,11 @@ public class GestionPartiesImpl implements GestionParties {
 		partie = partieDao.rechercher(partie.getNom());
 		//List<Carte> cartes = carteDao.lister();
 		List<JoueurPartie> joueurs = partieDao.listerJoueursPartie(partie);
-		for (int y=0; y<joueurs.size();y++) {
+		Iterator<JoueurPartie> ite= joueurs.iterator();
+		while (ite.hasNext()) {
+			JoueurPartie joueurPartie = ite.next();
 			for (int i = 0; i < 3; i++) {
-					joueurs.get(y).getMainsCarte().add(partieDao.piocher(partie));
+					joueurPartie.getMainsCarte().add(partieDao.piocher(partie));
 			}
 		}
 		return partieDao.mettreAJour(partie);
@@ -214,8 +217,6 @@ public class GestionPartiesImpl implements GestionParties {
 		for (JoueurPartie joueurPartie : joueurs) {
 			for (int i = 0; i < 4; i++) {
 				De de = des.remove(0);
-				de = deDao.initFaces(de);
-				System.out.println("faces du dé "+de.getId()+": "+de.getValeur()); 
 				joueurPartie.getMainsDe().add(de);
 				joueurPartieDaoImpl.mettreAJour(joueurPartie);
 			}
